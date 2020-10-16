4967136c67c2b204afd23336ce67cb8b5e3acb6a
Ryan Wilson <ryan.wilson@empiriolabs.io>
2020-10-16 11:10:43 -0400

    initial commit

---
+++ src/ProblemSet7.java
@@ -0,0 +1,99 @@
+public class ProblemSet7 {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * Exercise 1.
+     *
+     * Given two strings, return a new string built by surrounding in with the first
+     * and last two characters of out.
+     */
+    public String surroundMe(String in, String out) {
+
+    }
+
+    /*
+     * Exercise 2.
+     *
+     * Given a string and an integer, return a new string that represents the first
+     * and last n characters of text (overlapping, as needed).
+     */
+    public String endsMeet(String text, int n) {
+
+    }
+
+    /*
+     * Exercise 3.
+     *
+     * Given a string, return a new string using the middle three characters of text.
+     */
+    public String middleMan(String text) {
+
+    }
+
+    /*
+     * Exercise 4.
+     *
+     * Given two strings, determine whether or not target is equivalent to the middle
+     * three characters of text.
+     */
+    public boolean isCentered(String text, String target) {
+
+    }
+
+    /*
+     * Exercise 5.
+     *
+     * Given a string and a character, compute the number of words that end in suffix.
+     */
+    public int countMe(String text, char suffix) {
+
+    }
+
+    /*
+     * Exercise 6.
+     *
+     * Given a string, compute the number of triplets in text.
+     */
+    public int triplets(String text) {
+
+    }
+
+    /*
+     * Exercise 7.
+     *
+     * Given a string, compute the sum of the digits in text.
+     */
+    public long addMe(String text) {
+
+    }
+
+    /*
+     * Exercise 8.
+     *
+     * Given a string, compute the length of the longest sequence.
+     */
+    public long sequence(String text) {
+
+    }
+
+    /*
+     * Exercise 9.
+     *
+     * Given two strings, return a new string built by intertwining each of the
+     * characters of a and b.
+     */
+    public String intertwine(String a, String b) {
+
+    }
+
+    /*
+     * Exercise 10.
+     *
+     * Given a string, determine whether or not it is a palindrome.
+     */
+    public boolean isPalindrome(String text) {
+
+    }
+}
